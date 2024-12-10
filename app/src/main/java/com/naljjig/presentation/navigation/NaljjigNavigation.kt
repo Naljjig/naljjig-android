802bb463a7845f820eaa0fc52c41710e83f19ffc
kamja0510 <kamja990510@gmail.com>
2024-12-11 02:41:59 +0900

    [feature] #9 기본 navigation 생성

---
+++ app/src/main/java/com/naljjig/presentation/navigation/NaljjigNavigation.kt
@@ -0,0 +1,38 @@
+package com.naljjig.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.naljjig.presentation.home.HomeScreen
+import com.naljjig.presentation.login.LoginScreen
+import com.naljjig.presentation.signup.SignUpScreen
+import com.naljjig.presentation.welcome.WelcomeScreen
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Routes.WelcomeScreen
+    ) {
+        composable<Routes.WelcomeScreen> {
+            WelcomeScreen(
+
+            )
+        }
+
+        composable<Routes.LoginScreen> {
+            LoginScreen()
+        }
+
+        composable<Routes.SignUpScreen> {
+            SignUpScreen()
+        }
+
+        composable<Routes.HomeScreen> {
+            HomeScreen()
+        }
+    }
+}
